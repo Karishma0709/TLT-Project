3e7c5b50d68abe8899454c7c0fa068d8e72c1dfc
Neha patankar <84632833+Neha-patankar@users.noreply.github.com>
2024-07-30 10:54:02 +0530

    ipm

--- backend/models/personalInfo.js
+++ backend/models/personalInfo.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose")
     const Schema =mongoose.Schema
-
-
-    const personalInfoSchema=new Schema({
+const personalInfoSchema=new Schema({
         name: {
             type: String,
             required: true,
