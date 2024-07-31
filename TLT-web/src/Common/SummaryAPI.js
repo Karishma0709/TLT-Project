78b8866c83b600007d007f792644f8675fa00c65
Karishma <karishmadawar123@gmail.com>
2024-07-31 10:57:32 +0530

    imp

--- TLT-web/src/Common/SummaryAPI.js
+++ TLT-web/src/Common/SummaryAPI.js
@@ -1,82 +1,87 @@
 const backendDomain = "http://localhost:5050";
 
 const SummaryApi = {
-  fastTrackForm: {
-    url: `${backendDomain}/api/fastTrackForm`,
-    method: "post",
-  },
 
-  empowermentForm: {
-    url: `${backendDomain}/api/empowermentForm`,
-    method: "post",
-  },
-
-  jetForm: {
-    url: `${backendDomain}/api/jetForm`,
-    method: "get",
-  },
-  
-  personalinfo: {
-    url: `${backendDomain}/api/personalinfo`,
-    method: "post",
-  },
-
-  contactForm: {
-    url: `${backendDomain}/api/contactForm`,
-    method: "get",
-  },
-  mpcjForm: {
-    url: `${backendDomain}/api/mpcjForm`,
-    method: "post",
-  },
   tpmForm: {
     url: `${backendDomain}/api/tpmForm`,
-    method: "get",
-  },
-
-  prepaper: {
-    url: `${backendDomain}/api/prepaper`,
     method: "post",
   },
+
   pyPaper: {
     url: `${backendDomain}/api/pyPaper-info`,
     method: "get",
   },
 
-  saveGuardianDetails: {
-    url: `${backendDomain}/api/saveGuardianDetails`,
-    method: "post",
-  },
-  saveGuardianDetails: {
-    url: `${backendDomain}/api/saveGuardianDetails`,
-    method: "get",
-  },
+  // fastTrackForm: {
+  //   url: `${backendDomain}/api/fastTrackForm`,
+  //   method: "post",
+  // },
 
-  saveContactDocumentDetails: {
-    url: `${backendDomain}/api/saveContactDocumentDetails`,
-    method: "post",
-  },
-  saveContactDocumentDetails: {
-    url: `${backendDomain}/api/saveContactDocumentDetails`,
-    method: "get",
-  },
+  // empowermentForm: {
+  //   url: `${backendDomain}/api/empowermentForm`,
+  //   method: "post",
+  // },
 
-  EducationalDetails: {
-    url: `${backendDomain}/api/EducationalDetails`,
-    method: "post",
-  },
-  EducationalDetails: {
-    url: `${backendDomain}/api/EducationalDetails`,
-    method: "get",
-  },
+  // jetForm: {
+  //   url: `${backendDomain}/api/jetForm`,
+  //   method: "get",
+  // },
+  
+  // personalinfo: {
+  //   url: `${backendDomain}/api/personalinfo`,
+  //   method: "post",
+  // },
 
-  consentdetails: {
-    url: `${backendDomain}/api/consentdetails`,
-    method: "post",
-  },
-  ConsentDetails: {
-    url: `${backendDomain}/api/ConsentDetails`,
-    method: "get",
-  },
+  // contactForm: {
+  //   url: `${backendDomain}/api/contactForm`,
+  //   method: "get",
+  // },
+  // mpcjForm: {
+  //   url: `${backendDomain}/api/mpcjForm`,
+  //   method: "post",
+  // },
+ 
+
+  // prepaper: {
+  //   url: `${backendDomain}/api/prepaper`,
+  //   method: "post",
+  // },
+  
+
+  // saveGuardianDetails: {
+  //   url: `${backendDomain}/api/saveGuardianDetails`,
+  //   method: "post",
+  // },
+  // saveGuardianDetails: {
+  //   url: `${backendDomain}/api/saveGuardianDetails`,
+  //   method: "get",
+  // },
+
+  // saveContactDocumentDetails: {
+  //   url: `${backendDomain}/api/saveContactDocumentDetails`,
+  //   method: "post",
+  // },
+  // saveContactDocumentDetails: {
+  //   url: `${backendDomain}/api/saveContactDocumentDetails`,
+  //   method: "get",
+  // },
+
+  // EducationalDetails: {
+  //   url: `${backendDomain}/api/EducationalDetails`,
+  //   method: "post",
+  // },
+  // EducationalDetails: {
+  //   url: `${backendDomain}/api/EducationalDetails`,
+  //   method: "get",
+  // },
+
+  // consentdetails: {
+  //   url: `${backendDomain}/api/consentdetails`,
+  //   method: "post",
+  // },
+  // ConsentDetails: {
+  //   url: `${backendDomain}/api/ConsentDetails`,
+  //   method: "get",
+  // },
 };
 export default SummaryApi;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,38 +1,44 @@
 
 const express = require('express');
-const { createRegistrationForm } = require('../controllers/registrationFormController');
-const { createPersonalInfo, getPersonalInfo } = require('../controllers/personalInfoController');
-const { saveGuardianDetails } = require("../controllers/gaurdianDetailsController"); // Fixed typo
-const { saveContactDocumentDetails, findContactDocumentDetails } = require("../controllers/contactDocController");
-const { saveEducationalDetails, findEducationalDetails } = require("../controllers/educationalController");
-const { saveConsentDetails, findConsentDetails } = require("../controllers/consentController");
 const createPyPapersDetail = require('../controllers/pyPaperController');
-const { saveMPCJFormDetails, findMPCJFormDetails } = require('../controllers/mpcjOfflineController');
+const newTpmFormDetails =require("../controllers/tpmController")
+
+// const { createRegistrationForm } = require('../controllers/registrationFormController');
+// const { createPersonalInfo, getPersonalInfo } = require('../controllers/personalInfoController');
+// const { saveGuardianDetails } = require("../controllers/gaurdianDetailsController"); // Fixed typo
+// const { saveContactDocumentDetails, findContactDocumentDetails } = require("../controllers/contactDocController");
+// const { saveEducationalDetails, findEducationalDetails } = require("../controllers/educationalController");
+// const { saveConsentDetails, findConsentDetails } = require("../controllers/consentController");
+// const { saveMPCJFormDetails, findMPCJFormDetails } = require('../controllers/mpcjOfflineController');
 
 
 const router = express.Router();
 router.post('/prepaper',createPyPapersDetail );
 
-router.post('/consentdetails', saveConsentDetails); // Consistent naming
-router.get('/consent-details', findConsentDetails); // Consistent naming
-
-router.post("/mpcjForm",saveMPCJFormDetails)
+router.post('/tpmForm',newTpmFormDetails);
 
 
-router.post('/fastTrackForm', createRegistrationForm);
 
-router.post('/personalinfo', createPersonalInfo);
-router.get('/personal-info', getPersonalInfo);
+// router.post('/consentdetails', saveConsentDetails); // Consistent naming
+// router.get('/consent-details', findConsentDetails); // Consistent naming
+
+// router.post("/mpcjForm",saveMPCJFormDetails)
 
 
-router.post('/guardian-details', saveGuardianDetails); // Consistent naming
-router.get('/guardian-details', saveGuardianDetails); // This should probably have a corresponding GET controller if needed
+// router.post('/fastTrackForm', createRegistrationForm);
 
-router.post('/contact-document-details', saveContactDocumentDetails); // Consistent naming
-router.get('/contact-document-details', findContactDocumentDetails); // Consistent naming
+// router.post('/personalinfo', createPersonalInfo);
+// router.get('/personal-info', getPersonalInfo);
 
-router.post('/educational-details', saveEducationalDetails); // Consistent naming
-router.get('/educational-details', findEducationalDetails); // Consistent naming
+
+// router.post('/guardian-details', saveGuardianDetails); // Consistent naming
+// router.get('/guardian-details', saveGuardianDetails); // This should probably have a corresponding GET controller if needed
+
+// router.post('/contact-document-details', saveContactDocumentDetails); // Consistent naming
+// router.get('/contact-document-details', findContactDocumentDetails); // Consistent naming
+
+// router.post('/educational-details', saveEducationalDetails); // Consistent naming
+// router.get('/educational-details', findEducationalDetails); // Consistent naming
 
 
 
