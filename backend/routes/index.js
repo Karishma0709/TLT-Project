669dcb7d4d2a9ca410420582d6ad0acaa0463e9e
Karishma <karishmadawar123@gmail.com>
2024-07-31 11:56:25 +0530

    imp

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { saveTpmFormDetails, findTpmFormDetails } = require('../controllers/tpmController');
 const createPyPapersDetail = require('../controllers/pyPaperController');
+const { saveMPCJFormDetails, findMPCJFormDetails } = require('../controllers/mpcjOfflineController');
 
 // Define routes
 router.post('/prepaper', createPyPapersDetail);
 router.post('/tpmForm', saveTpmFormDetails);
+router.post("/mpcjForm", saveMPCJFormDetails);
+
 router.get('/tpmForm', findTpmFormDetails);
 
 module.exports = router;
