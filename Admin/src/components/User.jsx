12a4702238ecb45aed836418182466c557e39163
Neha patankar <84632833+Neha-patankar@users.noreply.github.com>
2024-08-06 10:14:36 +0530

    impo

---
+++ Admin/src/components/User.jsx
@@ -0,0 +1,83 @@
+import React, { useEffect, useState } from 'react'
+import { toast } from 'react-toastify'
+import moment from 'moment'
+// import { MdModeEdit } from "react-icons/md";
+// import ChangeUserRole from '../components/ChangeUserRole';
+import SummaryApi from '../Common/SummaryApi';
+
+const User = () => {
+    const [allUser,setAllUsers] = useState([])
+    const [openUpdateRole,setOpenUpdateRole] = useState(false)
+    const [updateUserDetails,setUpdateUserDetails] = useState({
+        email : "",
+        name : "",
+        // role : "",
+        // _id  : ""
+    })
+
+    const fetchAllUsers = async() =>{
+        const fetchData = await fetch(SummaryApi.AllUser.url,{
+            method : SummaryApi.AllUser.method,
+            credentials : 'include'
+        })
+
+        const dataResponse = await fetchData.json()
+
+        if(dataResponse.success){
+            setAllUsers(dataResponse.data)
+        }
+
+        if(dataResponse.error){
+            toast.error(dataResponse.message)
+        }
+
+    }
+
+    useEffect(()=>{
+        fetchAllUsers()
+    },[])
+
+  return (
+    <div className='bg-white pb-4'>
+        <table className='w-full userTable'>
+            <thead>
+                <tr className='bg-black text-white'>
+                    <th>Sr.</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Created Date</th>
+                </tr>
+            </thead>
+            <tbody className=''>
+                {
+                    allUser.map((el,index) => {
+                        return(
+                            <tr key={index}>
+                                <td>{index+1}</td>
+                                <td>{el?.name}</td>
+                                <td>{el?.email}</td>
+                                <td>{el?.role}</td>
+                                <td>{moment(el?.createdAt).format('LL')}</td>
+                                <td>
+                                    <button className='bg-green-100 p-2 rounded-full cursor-pointer hover:bg-green-500 hover:text-white' 
+                                    onClick={()=>{
+                                        setUpdateUserDetails(el)
+                                        setOpenUpdateRole(true)
+
+                                    }}
+                                    >
+                                
+                                    </button>
+                                </td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default User
