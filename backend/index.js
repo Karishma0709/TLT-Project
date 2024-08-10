6d8bcc689a37d237f676b8bf85508c5d26ff534c
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-08-10 16:36:23 +0530

    remove

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,4 @@
-// const express = require('express');
-// const cors = require('cors');
-// const cookieParser = require('cookie-parser');
-// require('dotenv').config();
-// const connectDB = require('./config/db');
-// const bodyParser = require('body-parser');
-// const router = require("./routes");
-//  // Import the registration routes
-// const app = express();
 
-// // Middleware
-// app.use(cors({
-//    origin: process.env.FRONTEND_URL,
-  credentials: true
-// }));
-// app.use(express.json());
-// app.use(bodyParser.json());
-// app.use(cookieParser());
-
-// app.use(express.urlencoded({extended:false}))
-// // Routes
-// app.use('/api', router);
-
-// const PORT = process.env.PORT || 5050; // or any other available port
-
-// connectDB()
-//   .then(() => {
-//     app.listen(PORT, () => {
-//       console.log('Connected to DB');
-//       console.log(`Server is running on port.... ${PORT}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.error('Failed to connect to DB', err);
-//   });
 
 const express = require('express');
 const cors = require('cors');
@@ -46,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174','http://localhost:5175', 'https://tlt-project-1.onrender.com'] ,// Update with your frontend origin
+  origin: ['http://localhost:5173', 'http://localhost:5174','http://localhost:5175',] ,// Update with your frontend origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
