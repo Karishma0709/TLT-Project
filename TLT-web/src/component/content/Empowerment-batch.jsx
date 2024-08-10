d4bf08f3d13db923fa2b520dbf02065d164a98c9
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-08-10 15:31:23 +0530

    add backend link

--- TLT-web/src/component/content/Empowerment-batch.jsx
+++ TLT-web/src/component/content/Empowerment-batch.jsx
@@ -63,7 +63,7 @@ const handleSubmit = async (e) => {
   // });
 
   try {
-    const response = await fetch('http://localhost:5050/api/empowermentForm', {
+    const response = await fetch('https://tlt-project-backend.onrender.com/api/empowermentForm', {
       method: 'POST',
       body: formData,
     });
