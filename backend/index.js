863dc21a58463e9f375d86d4dfbac46e38649e0e
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-08-12 13:42:58 +0530

    localhost

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,7 @@ const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
-// const router = require("./routes"); // Import the registration routes
-const routes = require('./routes')
+const router = require("./routes"); // Import the registration routes
 
 const app = express();
 
@@ -15,7 +14,7 @@ const app = express();
 app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:5174','http://localhost:5175',] ,// Update with your frontend origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  credentials: true
 }));
 app.use(express.json());
 app.use(bodyParser.json());
@@ -23,9 +22,9 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-app.use('/api', routes);
+app.use('/api', router);
 
-const PORT = process.env.PORT || 5053; // Changed port number
+const PORT = process.env.PORT || 5054; // Changed port number
 
 connectDB()
   .then(() => {
