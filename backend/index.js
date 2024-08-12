99b4ef8ca7df3ed5ba37f4d771a302977265bee8
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-08-12 11:56:43 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -11,11 +11,7 @@ const router = require("./routes"); // Import the registration routes
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174','http://localhost:5175',] ,// Update with your frontend origin
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
