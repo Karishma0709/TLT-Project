7452d503a0dc3a075b45206c45a327f3292cf7e9
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-08-13 18:11:18 +0530

    SET UP FOR ADMIN DEPLOY

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,14 @@ require('dotenv').config();
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const router = require("./routes"); // Import the registration routes
+const ADMIN_URL = process.env.ADMIN_URL
+
 
 const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http:`//localhost:5174','http://localhost:5175',] ,// Update with your frontend origin
+  origin: ['http://localhost:5173','http://localhost:5175',`${ADMIN_URL}`] ,// Update with your frontend origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
