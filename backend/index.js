7258cbf1cb6460680ba006d6a3619bda04496eee
AishwaryaParihar <toshiparihar121212@gmail.com>
2024-08-13 16:11:40 +0530

    add get for root

--- backend/index.js
+++ backend/index.js
@@ -5,21 +5,27 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const connectDB = require('./config/db');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const router = require("./routes"); // Import the registration routes
 
 const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174','http://localhost:5175',] ,// Update with your frontend origin
+  origin: ['http://localhost:5173', 'http:`//localhost:5174','http://localhost:5175',] ,// Update with your frontend origin
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
 app.use(express.json());
-// app.use(bodyParser.json());
+app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  res.send('Server is running!');
+});
+
+
 
 // Routes
 app.use('/api', router);
