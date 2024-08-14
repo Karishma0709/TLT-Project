cc2efcccf81080f4a722d9c4eb33373bdeb19164
testuser <testuser@gmail.com>
2024-08-14 17:01:53 +0530

    login and notification

--- Admin/src/components/Notification.jsx
+++ Admin/src/components/Notification.jsx
@@ -8,7 +8,7 @@ const Notification = () => {
      <label className='font-semibold pb-2' htmlFor="add">Marquee:</label>
     <textarea name="add" id="" placeholder='write your marquee' className='w-full p-3 max-h-svh border border-black' ></textarea>
     <div className=' text-center'>
-    <button className='p-2 rounded-md my-4 font-semibold shadow-red-300 shadow'>Submit</button>
+    <button className='p-2 rounded-md my-4 font-semibold shadow-red-300 shadow'>Update</button>
     </div>
 
     </form>
--- backend/models/notify.js
+++ backend/models/notify.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const notifySchema = new mongoose.Schema({
    notification:String,
-   
+   url:String
 });
 
 const Notify = mongoose.model('Notify', notifySchema);
