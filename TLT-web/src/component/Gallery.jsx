2a2b7c4c0d85faffce1cae2cdd72fceacde57ab0
Karishma <karishmadawar123@gmail.com>
2024-08-22 13:27:51 +0530

    galler updates and Unpaid page

--- TLT-web/src/component/Gallery.jsx
+++ TLT-web/src/component/Gallery.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Slider from './content/Slider'
+import Breadcrumbs from "./utiliti/breadcrumbs/Breadcrumbs";
 
 
 const Gallery = () => {
   return (
     <div className=''>
+      <Breadcrumbs   heading={'Gallery'}>
       <Slider/>
+      </Breadcrumbs>
     </div>
   )
 }
---
+++ TLT-web/src/component/data/unpaid.js
@@ -0,0 +1,55 @@
+import pdf1 from "../../assets/Unpaid/pdf1.pdf"
+
+const updaidProduct=[{
+    "id": 1,
+    "name": "Constitution of India - Hindi",
+    "pdf":"pdf1"
+},
+{
+    "id": 2,
+    "name": "Constitution of India - English",
+    "pdf":"hhg"
+},
+{
+    "id": 3,
+    "name": "Code of civil procedure - Hindi",
+    "pdf":"hhg"
+},
+{
+    "id": 4,
+    "name": "Code of civil procedure - English",
+    "pdf":"hhg"
+},
+{
+    "id": 5,
+    "name": "Code of criminal procedure - Hindi",
+    "pdf":"hhg"
+},
+{
+    "id": 6,
+    "name": "Code of criminal procedure - English",
+    "pdf":"hhg"
+},
+{
+    "id": 7,
+    "name": "Indian Evidence Act- Hindi",
+    "pdf":"hhg"
+},
+{
+    "id": 8,
+    "name": "Indian Evidence Act- English",
+    "pdf":"hhg"
+},
+{
+    "id": 9,
+    "name": "Indian Evidence Act- Hindi",
+    "pdf":"hhg"
+},
+{
+    "id": 10,
+    "name": "Indian Evidence Act- English",
+    "pdf":"hhg"
+},
+]
+
+export default updaidProduct
