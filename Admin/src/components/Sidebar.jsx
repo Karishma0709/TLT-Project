ee8de6ee774e25c8285bbfae46f976ad167c11bb
adityasinghchouhan1 <asadityasinghchouhan2072000@gmail.com>
2024-08-22 17:41:21 +0530

    py paper pdf

--- Admin/src/components/Sidebar.jsx
+++ Admin/src/components/Sidebar.jsx
@@ -39,6 +39,7 @@ const Sidebar = () => {
                     <nav className='grid p-4'>
                         <Link to={"user"} className='px-2 py-1 hover:bg-slate-100 hover:text-black hover:font-semibold'>All Users</Link>
                         <Link to={"py-paper"} className='px-2 py-1 hover:bg-slate-100 hover:text-black hover:font-semibold'>Previou Year Paper</Link>
+                        <Link to={"Prevyearpaperpdf"} className='px-2 py-1 hover:bg-slate-100 hover:text-black hover:font-semibold'>Previou Year Paper Pdf</Link>
                         <Link to={"mpcj-form"} className='px-2 py-1 hover:bg-slate-100 hover:text-black hover:font-semibold'>MPCJ form</Link>
                         <Link to={"tpm-form"} className='px-2 py-1 hover:bg-slate-100 hover:text-black hover:font-semibold'>TPM form</Link>
                         <Link to={"update-headline"} className='px-2 py-1 hover:bg-slate-100 hover:text-black hover:font-semibold'>HeadLine</Link>
---
+++ backend/controllers/PyPaperPdf.js
@@ -0,0 +1,14 @@
+const multer=require("multer")
+
+const upload= multer({dest:"uploads/"})
+ 
+async  function PyPaperPDF(req,res){
+upload.single("paperpdf")
+
+console.log(req.body)
+console.log(req.file)
+return res.redirect('/PyPaperPDF')
+}
+
+
+module.exports = PyPaperPDF;
